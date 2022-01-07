c9dd482ebe44ee7afd2040cfec51d76804557800
yeffry <yeffry45@gmail.com>
2022-01-07 12:02:32 -0500

    add basic webserver to house game data

---
+++ app_backend/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const axios = require("axios")
+const cors = require('cors')
+
+app.use(cors())
+app.use(express.json())
+
+// GET ALL GAMES
+
+app.get('/api/games', (req, res) => {
+  axios('https://api.rawg.io/api/games?key=391adff7676142c2b6d7314dcfda9a19')
+  .then((response) => {
+    res.json(response.data)
+  })
+})
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => {
+  console.log(`App is running on port ${PORT}`)
+})
