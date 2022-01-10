098c81254753a572b558f53fa4214fcb45609d14
yeffry <yeffry45@gmail.com>
2022-01-10 18:03:55 -0500

    add backend fetching data based on id

--- app_backend/server.js
+++ app_backend/server.js
@@ -15,6 +15,14 @@ app.get('/api/games', (req, res) => {
   })
 })
 
+app.get('/api/games/:id', (req, res) => {
+  const id = Number(req.params.id)
+  axios(`https://api.rawg.io/api/games/${id}?key=391adff7676142c2b6d7314dcfda9a19`)
+  .then((response) => {
+    res.json(response.data)
+  })
+})
+
 const PORT = process.env.PORT || 3000
 
 app.listen(PORT, () => {
